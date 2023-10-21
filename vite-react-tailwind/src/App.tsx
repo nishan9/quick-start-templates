31c6e4401d59de9a2cde2628d5f89a31a0a025b2
nishan.mahamalimage2 <nishan.mahamalimage2@sky.uk>
2023-10-21 11:22:34 +0100

    clean

--- vite-react-tailwind/src/App.tsx
+++ vite-react-tailwind/src/App.tsx
@@ -2,31 +2,9 @@ import './App.css'
 
 function App() {
 
-  const colors = ['border-[#FF5733]', '#33FF57', '#5733FF', '#33A6FF', '#FF33A6'];
-  //https://tailwindcss.com/docs/content-configuration#dynamic-class-names
   return (
     <>
-    {// ThIS WON'T WORK
-    }
-    <div className={`p-10 m-10 border-2 border-[${colors[2]}]`} >
-      <p>Hubble</p>
-    </div>
-    {// Pass the entire class instead like border-[#FF5733]
-    }
-    <div className={`p-10 m-10 border-2 ${colors[0]}`} >
-      <p>Hubble</p>
-    </div>
-     {// Natively in CSS
-     }
-    <div className="p-10 m-10 border-2" style={{ borderColor : `${colors[1]}` }}>
-      <p>Hubble</p>
-    </div>
-    <div className="p-10 m-10 border-2" style={{ borderColor : `${colors[2]}` }}>
-      <p>Hubble</p>
-    </div>
-    <div className="p-10 m-10 border-2" style={{ borderColor : `${colors[3]}` }}>
-      <p>Hubble</p>
-    </div>
+    
     </>
   )
 }
